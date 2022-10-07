2e510c1f705f6d9e60379bea31a5ad50ad535c04
jjkyuldashov <99941470+jjkyuldashov@users.noreply.github.com>
2022-10-07 10:29:18 +0500

    file upload

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,21 @@ import { Module } from '@nestjs/common';
 import { ZipcodeModule } from './zipcodes/zipcode.module';
 import { AuthModule } from './auth/auth.module';
 import { FileUploadController } from './file/file.upload.controller';
+import * as path from 'path';
+import { ServeStaticModule } from '@nestjs/serve-static';
 
 @Module({
-  imports: [ZipcodeModule, AuthModule],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: path.join(__dirname, '../uploads'),
+      serveRoot: '/uploads',
+      serveStaticOptions: {
+        index: false,
+      },
+    }),
+    ZipcodeModule,
+    AuthModule,
+  ],
   controllers: [FileUploadController],
   providers: [],
 })
