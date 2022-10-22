2b8d16c2e9640d5a85761151861075c3e4fc4323
jjkyuldashov <99941470+jjkyuldashov@users.noreply.github.com>
2022-10-22 17:03:50 +0500

    email

--- src/email/email.controller.ts
+++ src/email/email.controller.ts
@@ -1,39 +1,38 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import * as nodemailer from 'nodemailer';
 
 @Controller('email')
 export class EmailController {
-  //{
-  //     service: 'Gmail',
-  //     auth: {
-  //       type: 'OAuth2',
-  //       user: 'user@example.com',
-  //       clientId: '000000000000-xxx0.apps.googleusercontent.com',
-  //       clientSecret: 'XxxxxXXxX0xxxxxxxx0XXxX0',
-  //       refreshToken: '1/XXxXxsss-xxxXXXXXxXxx0XXXxxXXx0x00xxx',
-  //       accessToken: 'ya29.Xx_XX0xxxxx-xX0X0XxXXxXxXXXxX0x',
-  //       expires: 1484314697598,
-  //     },
-  //   }
-
   @Post('/send')
   async sendEmail(@Body() body: any) {
-    console.log('asdasd');
-
-    const {
-      smtp: { host, port, secure },
-      user,
-      pass,
-    } = await nodemailer.createTestAccount();
     const transporter = nodemailer.createTransport({
-      host,
-      port,
-      secure,
-      auth: { user, pass },
+      service: 'gmail',
+      auth: {
+        type: 'OAuth2',
+        user: 'safeed12341@gmail.com',
+        clientId:
+          '1037149241439-761olb4c8bppja86cqbrcbifm5qtigja.apps.googleusercontent.com',
+        clientSecret: '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',
+        refreshToken:
+          '1//04pFnCTsO31eqCgYIARAAGAQSNwF-L9IrEKu841Orjq3a_Zip8qkrOBisIXN5gt396L8ZrNYOSpzaeeiqsVcxjMxgKjdyBjB8cZ8',
+        accessToken:
+          'ya29.a0Aa4xrXOBI705pnwVj7KDul30jmlcrirwxgYvyaKduQXwdc4jSeDB1mCFfov6bxA8UkGarSOxpFHxloO_vXOfkR4VgwOv2JkD6uhc5g5zvvmhwAzbawNr1cB6eGIrPFeLpjsN54pi7HEEbnx-H5kEg73knd5AaCgYKATASARASFQEjDvL99dtEdxp8xFdcqVcAC1kb-g0163',
+        expires: 1484314697598,
+      },
     });
+    // var transporter = nodemailer.createTransport({
+    //   service: 'gmail',
+    //   type: "SMTP",
+    //   host: "smtp.gmail.com",
+    //   secure: true,
+    //   auth: {
+    //     user: 'safeed12341@gmail.com',
+    //     pass: 'Safeed123456'
+    //   }
+    // });
 
     const mailOptions = {
-      from: user,
+      from: 'safeed12341@gmail.com',
       to: body.email,
       subject: body.subject,
       html: body.body,
