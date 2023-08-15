135d0ed3f8fafa758637aa436f470c33d957f9a1
Jasurbek Yuldashov <jasurbekyuldashov123@gmail.com>
2023-08-15 21:12:41 +0500

    push

---
+++ src/quote/quote.controller.ts
@@ -0,0 +1,46 @@
+import { HttpService } from '@nestjs/axios';
+import {
+  Body,
+  Controller,
+  HttpCode,
+  HttpException,
+  HttpStatus,
+  Post,
+} from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+@Controller('post-quote')
+export class PostQuoteController {
+  constructor(
+    private configService: ConfigService,
+    private readonly httpService: HttpService,
+  ) {}
+
+  @Post('/')
+  @HttpCode(HttpStatus.OK)
+  async postQuote(@Body() body) {
+    try {
+      const url = this.configService.get<string>('url');
+      const api_key = this.configService.get<string>('token');
+      const data = await this.httpService.post(url, {
+        ...body,
+        api_key,
+      });
+      return {
+        message: 'Quote posted',
+        statusCode: HttpStatus.OK,
+        result: true,
+        data,
+      };
+    } catch (err) {
+      throw new HttpException(
+        {
+          message: 'Something went wrong',
+          error: err,
+          statusCode: HttpStatus.BAD_REQUEST,
+        },
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+  }
+}
